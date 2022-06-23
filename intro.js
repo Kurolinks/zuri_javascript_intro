ca1087082d9418402b852d04ab27d6a4af2156a1
Subiri Woyengikuro <kurolinks@gmail.com>
2022-06-23 22:58:34 +0100

    Removed prompts from code

--- intro.js
+++ intro.js
@@ -1,4 +1,1 @@
-// var name = prompt("What is your name: ");
-// var age = prompt("Please enter your age:", "Type here");
-// var height = propmy("What is your height in feets:")
-alert("My name is " + name + ", I am from " + country + " and I am " + height + " foot tall");
+alert("My name is Subiri Kuro. i am from Nigeria and i am 5 foot, 9 inches tall.");
