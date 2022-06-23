5ac147137aeec97f90c2244bf1be2856a55d8c6e
Subiri Woyengikuro <kurolinks@gmail.com>
2022-06-23 20:35:21 +0100

    Updated some lines

--- intro.js
+++ intro.js
@@ -1,13 +1,4 @@
-alert("my name is Subiri Woyengikuro, I am 6 foot tall and I am from Nigeria.")
-
-var year_born = prompt("Please enter your date of birth:", "Type here");
-var d = new Date();
-var n = d.getFullYear();
-function getAge(birthYear){
-	var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    age = currentYear - birthYear;
-    return age;
-}
-calculatedAge = getAge(year_born);
-alert("Hello, " + "you are " + calculatedAge + " years old!");
+// var name = prompt("What is your name: ");
+// var age = prompt("Please enter your age:", "Type here");
+// var height = propmy("What is your height in feets:")
+alert("My name is " + name + ", I am from " + country + " and I am " + height + " foot tall");
